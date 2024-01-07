bb1f5df76955477acf672173820f45ccad03a741
herybrts <115608706+herybrts@users.noreply.github.com>
2024-01-07 09:09:17 +0700

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 ################################################################################
 # Script for installing Odoo on Ubuntu 22.04 (could be used for other version too)
 # Author: Yenthe Van Ginneken
@@ -26,7 +26,7 @@ OE_PORT="8069"
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 13.0
 OE_VERSION="17.0"
 # Set this to True if you want to install the Odoo enterprise version!
-IS_ENTERPRISE="True"
+IS_ENTERPRISE="False"
 # Set this to True if you want to install Nginx!
 INSTALL_NGINX="True"
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
@@ -35,7 +35,7 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="http://216.126.231.159"
+WEBSITE_NAME="216.126.231.159"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
